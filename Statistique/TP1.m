5188757ba11515ad1dcfb08dfdc8780fae0044c6
Guillaume <guillaumeduret1@gmail.com>
2020-09-11 11:18:32 +0100

    bonne image de base

--- Statistique/TP1.m
+++ Statistique/TP1.m
@@ -1,4 +1,4 @@
-%% init
+%% read image
 clear all; close all; clc;
 
 img = im2double(imread('text0.png'));
@@ -10,12 +10,12 @@ title('image origine')
 %% patch init
 [w_true,l_true,c_true] = size(img)
 
-taille= 11%impair
+taille= 21%impair
 point_depart=[uint64(w_true/2),uint64(l_true/2)]
 patch = img((point_depart(1)-taille/2:point_depart(1)+taille/2-1),(point_depart(2)-taille/2:point_depart(2)+taille/2-1), :);
 figure()
 imshow(patch,[])
-title('patch')
+title('taille patch')
 
 
 %% swp_img init
@@ -26,9 +26,19 @@ swp_img = img%((point_depart(1)-swp_taille/2:point_depart(1)+swp_taille/2-1),(po
 %swp_img=img(:,:,1)
 [w_swp,l_swp,c_swp] = size(swp_img);
 
-size_modelized = 1;
+point_depart_modilized=[uint64(w_swp/2),uint64(l_swp/2)]
 
-base_modelized = swp_img;
+base_modelized = swp_img((point_depart_modilized(1)-swp_taille/2:point_depart_modilized(1)+swp_taille/2-1),(point_depart_modilized(2)-swp_taille/2:point_depart_modilized(2)+swp_taille/2-1),:);
+
+figure()
+imshow(base_modelized,[])
+title('image debut')
+
+%% base modelized
+
+size_modelized_x = 1;
+size_modelized_y = 1;
+size_modelized = 70;
 
 [w_base,l_base,c_base] = size(base_modelized);
 modelized_img = zeros(w_base+size_modelized*2+taille-1,l_base+size_modelized*2+taille-1,3);
@@ -43,7 +53,7 @@ for k=1:w_base
        
        %a=modilized_img(point_depart(1) - taille/2 + k +  (taille/2)  ,point_depart(2) - taille/2 + l +   (taille/2)   ,:)
        %b=patch(k,l,:)
-       modelized_img( (taille-1)/2 + size_modelized + k   , (taille-1)/2 + size_modelized + l ,: ) = swp_img(k,l,:);
+       modelized_img( (taille-1)/2 + size_modelized + k   , (taille-1)/2 + size_modelized + l ,: ) = base_modelized(k,l,:);
        filled_matrice( (taille-1)/2 + size_modelized + k  , (taille-1)/2 + size_modelized + l  ) = 1;
        %ab=modilized_img(point_depart(1) - taille/2 + k +  (taille/2)  ,point_depart(2) - taille/2 + l +   (taille/2)   ,:)
        compteur=compteur+1;
@@ -123,7 +133,7 @@ while compteur < fin
                    %res2 = modilized_img(good_i+k- (w_patch+1)/2,good_j+l- (l_patch+1)/2)
                    %res3 = int64(swp_img(k+a - (w_patch+1)/2,l+b - (l_patch+1)/2)) - int64(modilized_img(good_i+k- (w_patch+1)/2,good_j+l- (l_patch+1)/2))
                    if filled_matrice(good_i+k- (w_patch+1)/2,good_j+l- (l_patch+1)/2)
-                       Dist_patch = ( (base_modelized( k+a-(w_patch+1)/2 , l+b-(l_patch+1)/2 , :) - modelized_img( good_i+k-(w_patch+1)/2 , good_j+l-(l_patch+1)/2 ,:)).^2 )/max_neighboorood + Dist_patch;
+                       Dist_patch = ( (swp_img( k+a-(w_patch+1)/2 , l+b-(l_patch+1)/2 , :) - modelized_img( good_i+k-(w_patch+1)/2 , good_j+l-(l_patch+1)/2 ,:)).^2 )/max_neighboorood + Dist_patch;
                        G = G + filled_matrice(good_i+k- (w_patch+1)/2,good_j+l- (l_patch+1)/2);
                    end
                end
@@ -154,7 +164,7 @@ end
 %end 
 
 
-image_final = modelized_img((taille-1)/2+1:w_swp-(taille-1)/2 , (taille-1)/2+1:l_swp-(taille-1)/2 , :);
+image_final = modelized_img((taille-1)/2+1:w_mod-(taille-1)/2 , (taille-1)/2+1:l_mod-(taille-1)/2 , :);
 
 figure()
 subplot(131)
@@ -168,5 +178,10 @@ imshow(filled_matrice,[])
 title('matrice filled elements')
 
 figure()
+subplot(121)
 imshow(image_final,[])
 title('image_final')
+
+subplot(122)
+imshow(base_modelized,[])
+title('image_origine')
